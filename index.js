4d1ee2c2e5c0a082bb31539aa17e856bdbc710f7
Joshua Hodges <j.b.h.0411@gmail.com>
2020-08-24 18:02:47 +0100

    now returns youtube video id from api

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 require("dotenv").config()
 const GoogleImages = require("google-images");
 const { Client } = require("discord.js");
+const {google} = require('googleapis');
 
 const client = new Client()
 const googleImages = new GoogleImages(process.env.GOOGLE_SE_KEY, process.env.GOOGLE_API);
+
+const youtube = google.youtube({
+  version:'v3',
+  auth:process.env.YOUTUBE_KEY,
+})
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
@@ -26,8 +32,21 @@ client.on("message", msg => {
     });
   }
 
-  if (msg.content.startsWith(".yt")){
-    msg.channel.send("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+  else if (msg.content.startsWith(".yt")){
+    // Get arguments
+    const args = msg.content.slice(".yt".length).trim().split(' ')
+    var q = '';
+    args.forEach(element => {
+        q += element + ' '
+    });
+    console.log("Youtube search: " + q)
+
+    youtube.search.list({part:'id,snippet'}, {q: q, maxResults: 10})
+    .then( result =>{
+        console.log("Youtuibe video ID: " + result.data.items[1].id.videoId)
+        msg.channel.send("https://www.youtube.com/watch?v=" + result.data.items[1].id.videoId)
+      }
+    )
   }
 
 })
