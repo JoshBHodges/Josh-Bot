81f9497509465d1f2b06af9358e6e61efb24e30c
Joshua Hodges <j.b.h.0411@gmail.com>
2020-08-28 17:35:48 +0100

    added commands to help function

--- functions/help.js
+++ functions/help.js
@@ -1,6 +1,6 @@
 
 module.exports={
     help: function(msg){
-        msg.reply('Help has arrived');
+        msg.reply('\n **Help has arrived!** \n\n **Commands:** \n\t -?image "search query" \n\t -?yt "search query" \n\t -?clear "number of messages to clear"');
     }
 }
